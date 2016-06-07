1d01dc64edbe5f7b0e523a25de30d2040e5b8f1c
Jeferson Belmiro <jeferson.belmiro@gmail.com>
2016-06-07 13:57:34 -0300

    fix

--- game.c
+++ game.c
@@ -18,7 +18,7 @@
         abort();
 
 typedef struct {
-    int *grid;
+    int **grid;
     int rows;
     int cols;
 } Board;
@@ -29,22 +29,26 @@ typedef struct {
 
 int getch(void) 
 {
-    int c=0;
+    int key = 0;
 
     struct termios org_opts, new_opts;
-    int res=0;
-    //-----  store old settings -----------
+    int res = 0;
+
+    // store old settings
     res=tcgetattr(STDIN_FILENO, &org_opts);
-    assert(res==0);
-    //---- set new terminal parms --------
+    assert(res == 0);
+
+    // set new terminal parms 
     memcpy(&new_opts, &org_opts, sizeof(new_opts));
     new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
     tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
-    c=getchar();
-    //------  restore old settings ---------
-    res=tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
-    assert(res==0);
-    return(c);
+    key = getchar();
+
+    // restore old settings 
+    res = tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
+    assert(res == 0);
+
+    return(key);
 }
 
 void clear()
@@ -71,26 +75,6 @@ void get_winsize(int *rows, int *cols)
     *cols = w.ws_col;
 }
 
-int get_grid_pos(Board *board, int row, int col) 
-{
-    // sequencial 
-    return (row * board->cols + col);
-    // create 2d indices
-    // return (col * board->rows + row);
-}
-
-int get_grid_value(Board *board, int row, int col) 
-{
-    int id = get_grid_pos(board, row, col);
-    return board->grid[id];
-} 
-
-void set_grid_value(Board *board, int row, int col, int value) 
-{
-    int id = get_grid_pos(board, row, col);
-    board->grid[id] = value;
-}
-
 void update()
 {
 
@@ -101,23 +85,27 @@ void draw(Game * game)
     clear();
     for (int row = 0, it = 0; row < game->board->rows; row++) {
         for (int col = 0; col < game->board->cols; col++) {
-            draw_col(row, col, ".");
-            /* printf("[%d][%d] = %d\n", row, col, game->board->grid[it++]); */
+            /* draw_col(row, col, "."); */
+            printf("[%d][%d] = %d\n", row, col, game->board->grid[row][col]);
         }
     } 
 }
 
 Board *create_board(int rows, int cols)
 {
+    int **grid;
+
     Board * board = malloc(sizeof(Board));
-    board = board;
-    board->grid = malloc(rows * cols * sizeof(int));
+    grid = malloc(rows * cols * sizeof(int *));
+
+    board->grid = grid;
     board->rows = rows;
     board->cols = cols;
 
     for (int row = 0, it = 0; row < board->rows; row++) {
+        board->grid[row] = malloc(cols * sizeof(int));
         for (int col = 0; col < board->cols; col++) {
-            board->grid[it++] = 0;
+            board->grid[row][col] = col + row * rows;
         }
     }
 
@@ -154,22 +142,7 @@ int main(int argc, char **argv)
     Game *game = create_game(board);
 
     // loop game
-
-    int debug_row = 37;
-    int debug_col = 119;
-
-    set_grid_value(board, debug_row, debug_col, 3);
-    set_grid_value(board, 37, 150, 33);
-
     tick(game); 
-
-    printf(
-        "\ndebug: [%d][%d] >> %d = %d\n", 
-        debug_row, 
-        debug_col, 
-        get_grid_pos(board, debug_row, debug_col), 
-        get_grid_value(board, debug_row, debug_col)
-    );
 
     return 0;
 }
