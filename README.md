e80adfc6902163d2f0c05909c29d7eeb890492a7
Jeferson Belmiro <jeferson.belmiro@gmail.com>
2016-06-12 21:18:48 -0300

    fix

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # test
 
 https://github.com/harshjv/threes-c/blob/master/include/array.h
+
+dependence:
+```
+sudo apt-get install libncurses5-dev
+make
+bin/game
+```
--- src/game.c
+++ src/game.c
@@ -3,63 +3,35 @@
 #include <locale.h>
 #include <string.h>
 
-// lib for get size of terminal
-#include <sys/ioctl.h>
-#include <unistd.h>
+// PDF: http://heather.cs.ucdavis.edu/~matloff/UnixAndC/CLanguage/Curses.pdf
+#include <curses.h>
 
-// libs for getch() 
-#include <termios.h>
+// sleep
 #include <unistd.h>
-#include <assert.h>
-#include <string.h>
 
 #define fatal(msg)\
-        fprintf(stderr, "[error] line %d: %s\n", __LINE__, msg);\
-        abort();
+    fprintf(stderr, "[error] line %d: %s\n", __LINE__, msg);\
+abort();
 
 typedef struct {
-    int **grid;
+    char **grid;
     int rows;
     int cols;
 } Board;
 
 typedef struct {
+    int row;
+    int col;
+    bool fire;
+} Aim;
+
+typedef struct {
     Board *board;
+    int keypress;
 } Game; 
 
-int getch(void) 
-{
-    int key = 0;
-
-    struct termios org_opts, new_opts;
-    int res = 0;
-
-    // store old settings
-    res=tcgetattr(STDIN_FILENO, &org_opts);
-    assert(res == 0);
-
-    // set new terminal parms 
-    memcpy(&new_opts, &org_opts, sizeof(new_opts));
-    new_opts.c_lflag &= ~(ICANON | ECHO | ECHOE | ECHOK | ECHONL | ECHOPRT | ECHOKE | ICRNL);
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_opts);
-    key = getchar();
-
-    // restore old settings 
-    res = tcsetattr(STDIN_FILENO, TCSANOW, &org_opts);
-    assert(res == 0);
-
-    return(key);
-}
-
-void clear()
-{
-    printf("\033[2J\033[H");
-}
-
-void draw_col(int row, int col, char *str)
-{
-    printf("%c[%d;%df%s\n\r", 0x1B, row, col, str);
-}
+Game * game;
+Aim * aim;
 
 void set_current_user_locale()
 {
@@ -67,33 +39,124 @@ void set_current_user_locale()
     locale = setlocale(LC_ALL, "");
 }
 
-void get_winsize(int *rows, int *cols)
+void create_window(int *rows, int *cols)
 {
-    struct winsize w;
-    ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    *rows = w.ws_row;
-    *cols = w.ws_col;
+    WINDOW *wnd;
+
+    // curses call to initialize window
+    wnd = initscr(); 
+
+    // curses call to set no waiting for Enter key
+    cbreak(); 
+
+    // curses call to set no echoing
+    noecho(); 
+
+    // curses call to find size of window
+    getmaxyx(wnd, *rows, *cols); 
+
+    // curses call to clear screen, send cursor to position (0,0)
+    clear(); 
+
+    // curses call to implement all changes since last refresh
+    refresh(); 
+
+    // don't display a cursor
+    curs_set(false);
+
+    // enable keyboard mapping
+    keypad(stdscr, TRUE);
+
+    // input timeout
+    timeout(0);//1000/8);   
+}
+
+void restore_window()
+{
+    // curses call to restore the original window and leave
+    endwin(); 
+}
+
+void quit()
+{
+    restore_window();
+    exit(0);
+} 
+
+void update_aim()
+{
+    game->board->grid[aim->row][aim->col -1] = '[';
+    game->board->grid[aim->row][aim->col +1] = ']';
+}
+
+void draw_char(int row, int col, char value)
+{
+    move(row, col);
+    /* delch(); */
+    insch(value);
+
+    /* mvprintw(row, col, value);  */
 }
 
 void update()
 {
+    game->board->grid[aim->row][aim->col -1] = ' ';
+    game->board->grid[aim->row][aim->col +1] = ' ';
 
+    game->keypress = getch();
+
+    if (game->keypress == 'q') {
+        quit();
+    }
+
+    if (game->keypress == KEY_LEFT) {
+        aim->col -= 1;
+    }
+
+    if (game->keypress == KEY_RIGHT) {
+        aim->col += 1;
+    }
+
+    if (game->keypress == KEY_UP) {
+        aim->row -= 1;
+    }
+
+    if (game->keypress == KEY_DOWN) {
+        aim->row += 1;
+    }
+
+    if (aim->row > game->board->rows - 1) {
+        aim->row = game->board->rows - 1;
+    }
+
+    if (aim->col > game->board->cols - 2) {
+        aim->col = game->board->cols - 2;
+    }
+
+    if (aim->col < 1) {
+        aim->col = 1;
+    }
+    if (aim->row < 0) {
+        aim->row = 0;
+    }
+
+    update_aim();
 } 
 
-void draw(Game * game)
+void draw()
 {
-    clear();
+    erase();
     for (int row = 0, it = 0; row < game->board->rows; row++) {
         for (int col = 0; col < game->board->cols; col++) {
-            draw_col(row, col, ".");
-            /* printf("[%d][%d] = %d\n", row, col, game->board->grid[row][col]); */
+            draw_char(row, col, game->board->grid[row][col]);
         }
     } 
+    refresh();
 }
 
 Board *create_board(int rows, int cols)
 {
-    int **grid;
+    char **grid;
 
     Board * board = malloc(sizeof(Board));
     grid = malloc(rows * cols * sizeof(int *));
@@ -105,28 +168,30 @@ Board *create_board(int rows, int cols)
     for (int row = 0, it = 0; row < board->rows; row++) {
         board->grid[row] = malloc(cols * sizeof(int));
         for (int col = 0; col < board->cols; col++) {
-            board->grid[row][col] = col + row * rows;
+            board->grid[row][col] = ' ';
         }
     }
 
     return board;
 }
 
-void tick(Game *game)
+void tick()
 {
-    int iterations = 10;
-    while (iterations-- > 0) {
-    
+    int iterations = 10, fps = 1000/60;
+
+    do {
         update();
-        draw(game);
-        sleep(1);
-    }
+        draw();
+        /* usleep(fps); */
+    } while (true);
 }
 
 Game *create_game(Board *board)
 {
-    Game *game = malloc(sizeof(Game));
+    game = malloc(sizeof(Game));
     game->board = board;
+
+    aim = malloc(sizeof(Aim));
 
     return game;
 }
@@ -134,20 +199,18 @@ Game *create_game(Board *board)
 int main(int argc, char **argv)
 {
     set_current_user_locale(); 
-    
+
     // get terminal size
     int winsize_rows, winsize_cols;
-    get_winsize(&winsize_rows, &winsize_cols);
-
-    printf(" - hunter game\n");
-    printf(" - lines %d\n", winsize_rows);
-    printf(" - columns %d\n", winsize_cols);
+    create_window(&winsize_rows, &winsize_cols);
 
     Board *board = create_board(winsize_rows, winsize_cols);
-    Game *game = create_game(board);
+    create_game(board);
 
     // loop game
-    tick(game); 
+    tick(); 
+
+    quit();
 
     return 0;
 }
