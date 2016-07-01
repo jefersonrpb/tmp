94854717282e26fee0d1558c88519b097b8e4429
Jeferson Belmiro <jeferson.belmiro@gmail.com>
2016-07-01 01:19:40 -0300

    fix

--- inc/game.h
+++ inc/game.h
@@ -15,25 +15,25 @@ typedef struct {
 } Point;
 
 typedef struct {
-    Point position;
-    Point velocity;
+    Point *position;
     int direction;
 } Moto;
 
-enum {UP, DOWN, RIGHT, LEFT, WALL, EMPTY};
+enum {UP, DOWN, RIGHT, LEFT, WALL, EMPTY, GAME_OVER, PLAY, MENU};
 
-#define FPS 1000000 / 10
+#define SECOND 1000000
+#define FPS SECOND / 10
 
 void update();
 void draw();
 void tick();
-void game_over();
+void game_over(bool player_loses);
 void process_args();
 void set_locale();
 void create_window();
 void create_map();
 void create_players();
-Moto *new_player(int direction, int x, int y);
+Moto *new_player(int x, int y, int direction);
 int **create_positions(int length);
 void restore_window();
 void quit();
@@ -42,10 +42,13 @@ void fulfill_wall(Moto *player);
 void update_player();
 void draw_rectangle(int x, int y, int width, int height);
 void input_player_direction(Moto *player);
-void update_player_velocity(Moto *player);
 void update_player_position(Moto *player);
-void check_player_collision(Moto *player);
 void update_dumb_ai();
 int *get_allowed_directions(Moto *player);
+bool check_collision(Point *position);
+Point *get_next_position(Point *current_position, int direction);
+Point *new_point(int x, int y);
+void start();
+void create_menu();
 
 #endif //GAME_INCLUDED
--- src/game.c
+++ src/game.c
@@ -10,6 +10,9 @@ char **map;
 // key code
 int keypress;
 
+// game state
+int current_state;
+
 // sizes
 int screen_height, screen_width;
 int map_width, map_height; 
@@ -19,14 +22,33 @@ int max_players;
 int main(int argc, char **argv)
 {
     process_args(argc, argv);
-    // initialize random seed
-    srand (time(NULL));
-    set_locale(); 
     create_window();
+    create_menu(); 
+    set_locale(); 
+    srand(time(NULL));
+    tick();
+    return 0;
+}
+
+void start()
+{
     create_map();
     create_players();
-    tick(); 
-    return 0;
+    current_state = PLAY;
+}
+
+void create_menu()
+{
+    current_state = MENU;
+    draw_rectangle(0, 0, game_width-1, game_height-1);
+    int center_x = game_width/2;
+    mvprintw(1, center_x - 14, " _____   ___    ___    _  _ ");
+    mvprintw(2, center_x - 14, "|_   _| | _ \\  / _ \\  | \\| |");
+    mvprintw(3, center_x - 14, "  | |   |   / | (_) | | .` |");
+    mvprintw(4, center_x - 14, "  |_|   |_|_\\  \\___/  |_|\\_|");
+    mvprintw(8, center_x-9, "PRESS 'Q' TO QUIT"); 
+    mvprintw(10, center_x-2, "OR"); 
+    mvprintw(12, center_x-11, "ENTER TO START/RESTART"); 
 }
 
 void tick()
@@ -45,21 +67,30 @@ void update()
         quit();
     }
 
+    if (current_state != PLAY) {
+        if (keypress == KEY_ENTER || keypress == 10) {
+            start();
+        }
+        return;
+    }
+
     input_player_direction(ptr_players[0]);
     for (size_t i = 0; i < max_players; i++) {
         if (i > 0) update_dumb_ai(ptr_players[i]);
-        update_player_velocity(ptr_players[i]);
         update_player_position(ptr_players[i]);
-        check_player_collision(ptr_players[i]);
+        if (check_collision(ptr_players[i]->position)) return game_over(i == 0);
         fulfill_wall(ptr_players[i]);
     }
 } 
 
 void draw()
 {
+    if (current_state != PLAY) {
+        return;
+    }
     erase();
     draw_rectangle(0, 0, game_width-1, game_height-1);
-    mvprintw(0, 0, "%dx%d | %dx%d", ptr_players[0]->position.x, ptr_players[0]->position.y, map_width, map_height); 
+    /* mvprintw(0, 0, "%dx%d | %dx%d", ptr_players[0]->position->x, ptr_players[0]->position->y, map_width, map_height);  */
     for (int x = 0; x < map_width; x++) {
         for (int y = 0; y < map_height; y++) {
             draw_char(x, y, map[x][y]);
@@ -140,14 +171,14 @@ int **create_positions(int length)
     // @todo usar for, criar aleatorimanete posicoes
     /* for (size_t i = 0; i < length; i++) { } */
     positions[0] = malloc(sizeof(int) * 3);
-    positions[0][0] = RIGHT;
-    positions[0][1] = 0;
-    positions[0][2] = 3;
+    positions[0][0] = 0;
+    positions[0][1] = 3;
+    positions[0][2] = RIGHT;
 
     positions[1] = malloc(sizeof(int) * 3);
-    positions[1][0] = LEFT;
-    positions[1][1] = map_width - 1;
-    positions[1][2] = map_height - 3;
+    positions[1][0] = map_width - 1;
+    positions[1][1] = map_height - 4;
+    positions[1][2] = LEFT;
 
     return positions;
 }
@@ -162,16 +193,11 @@ void create_players()
     }
 }
 
-Moto *new_player(int direction, int x, int y)
+Moto *new_player(int x, int y, int direction)
 {
     Moto *player = malloc(sizeof(Moto));
-
+    player->position = new_point(x, y);
     player->direction = direction;
-    player->position.x = x;
-    player->position.y = y;
-    player->velocity.x = 0;
-    player->velocity.y = 0;
-
     return player;
 }
 
@@ -189,53 +215,79 @@ void quit()
 
 void draw_char(int x, int y, char value)
 {
-    if (value == WALL) {
-        mvaddch(y+1, x+1, ACS_BLOCK); 
-    }
+    if (value == WALL) mvaddch(y+1, x+1, ACS_BLOCK); 
 }
 
 void input_player_direction(Moto *player)
 {
-    if (keypress == KEY_LEFT) player->direction = LEFT;
-    if (keypress == KEY_RIGHT) player->direction = RIGHT;
-    if (keypress == KEY_UP) player->direction = UP;
-    if (keypress == KEY_DOWN) player->direction = DOWN;
-}
-
-void update_player_velocity(Moto *player)
-{
-    player->velocity.x = 0;
-    player->velocity.y = 0;
-    if (player->direction == RIGHT) player->velocity.x = +1;
-    if (player->direction == LEFT) player->velocity.x = -1;
-    if (player->direction == UP) player->velocity.y = -1;
-    if (player->direction == DOWN) player->velocity.y = +1;
+    int *directions = get_allowed_directions(player);
+    int direction = player->direction;
+    if (keypress == KEY_LEFT) direction = LEFT;
+    if (keypress == KEY_RIGHT) direction = RIGHT;
+    if (keypress == KEY_UP) direction = UP;
+    if (keypress == KEY_DOWN) direction = DOWN;
+    if (direction == directions[0] || direction == directions[1])  player->direction = direction;
 }
 
 void update_player_position(Moto *player)
 {
-    player->position.x += player->velocity.x;
-    player->position.y += player->velocity.y;
-}
-
-void check_player_collision(Moto *player)
-{
-    if (player->position.x < 0 || player->position.x >= map_width) game_over();
-    if (player->position.y < 0 || player->position.y >= map_height) game_over();
-    if (map[player->position.x][player->position.y] == WALL) game_over();
+    player->position = get_next_position(player->position, player->direction);
 }
 
 void fulfill_wall(Moto *player)
 {
-    map[player->position.x][player->position.y] = WALL;
+    map[player->position->x][player->position->y] = WALL;
+}
+
+Point *new_point(int x, int y)
+{
+    Point *position = malloc(sizeof(Point));
+    position->x = x;
+    position->y = y;
+    return position;
+}
+
+Point *get_next_position(Point *current_position, int direction)
+{
+    Point *position = new_point(current_position->x, current_position->y);
+    if (direction == RIGHT) position->x += 1;
+    if (direction == LEFT) position->x -= 1;
+    if (direction == UP) position->y -= 1;
+    if (direction == DOWN) position->y += 1;
+    return position;
+}
+
+bool check_collision(Point *position)
+{
+    if (position->x < 0 || position->x >= map_width) return true;
+    if (position->y < 0 || position->y >= map_height) return true;
+    if (map[position->x][position->y] == WALL) return true;
+    return false;
 }
 
 void update_dumb_ai(Moto *ai)
 {
-    int try_new_direction = rand() % 10;
-    int *directions = get_allowed_directions(ai);
-    if (try_new_direction == 8) ai->direction = directions[0]; 
-    if (try_new_direction == 9) ai->direction = directions[1]; 
+    bool try_new_direction = rand() % 10 == 8;
+    if (try_new_direction) {
+        int *directions = get_allowed_directions(ai);
+        ai->direction = rand() % 1 == 1 ? directions[0] : directions[1]; 
+
+        // new direction will collide, get adjacent
+        if (check_collision(get_next_position(ai->position, ai->direction))) {
+            ai->direction = ai->direction == directions[0] ? directions[1]: directions[0]; 
+        }
+        return;
+    }
+
+    // if will collide force new direction
+    if (check_collision(get_next_position(ai->position, ai->direction))) {
+
+        int *directions = get_allowed_directions(ai);
+        ai->direction = directions[0];
+        if (check_collision(get_next_position(ai->position, ai->direction))) {
+            ai->direction = directions[1];
+        }
+    }
 }
 
 int *get_allowed_directions(Moto *player)
@@ -256,10 +308,15 @@ int *get_allowed_directions(Moto *player)
     return directions; 
 }
 
-void game_over()
+void game_over(bool player_loses)
 {
-    sleep(2);
-    quit();
+    current_state = GAME_OVER;
+    char *text = " YOU LOST ";
+    if (!player_loses) {
+        text = " YOU WON ";
+    }
+    int x = (map_width / 2) - 5;
+    mvprintw(0, x, "%s", text); 
 }
 
 void draw_rectangle(int x, int y, int width, int height)
