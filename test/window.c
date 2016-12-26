99c3ad8068cd2403f5cc9978fc93290aaf0cd4dd
Jeferson Belmiro <jeferson.belmiro@gmail.com>
2016-12-26 04:24:01 -0200

    fix

--- test/window.c
+++ test/window.c
@@ -1,14 +1,27 @@
 #include "test.h"
 #include "window.h"
 
+#include <ncurses.h>
+
 Board* board = NULL;
 Window* window = NULL;
+
+static bool window_has_string(int x, int y, char* string)
+{
+    char buf[1024];
+    mvinnstr(y, x, buf, strlen(string));
+    return strcmp(string, buf) == 0;
+}
 
 void test_draw()
 {
     window_draw_string(0, 0, "test draw string");
     window_draw_char(5, 5, 'a');
     window_refresh();
+
+    ASSERT(window_has_string(0, 0, "test draw"));
+    ASSERT(window_has_string(10, 0, "string"));
+    ASSERT(window_has_string(5, 5, "a"));
 }
 
 void test_sizes()
